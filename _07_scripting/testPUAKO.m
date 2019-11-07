d2a9b776977d65cfb286b4732b5dc65750674a5c
obeltramo-martin <olivier.martin@lam.fr>
2019-11-07 12:15:38 -1000

    Add the script file

---
+++ _07_scripting/testPUAKO.m
@@ -0,0 +1,23 @@
+clear all;
+close all;
+
+%% DEFINE DATA PATH
+path_root = '/psfrData/NIRC2/';
+path_calib = '/psfrData/PUAKO_RESULTS/CALIBRATION/';
+path_save='/psfrData/PUAKO_RESULTS/RESULTS/';
+path_night = [path_root,'20130801/'];
+
+p = puako();
+
+%% Test data 
+if testData
+    p = p.grabData('id',{'n0004'},'path_night',path_night,'path_calibration',path_calib,'path_save',[path_save,'20130801/']);
+end
+
+%% Test PSFR
+if testPSFR
+    t0 = tic();
+    p.getRecPSF('id',{'all'},'path_night',path_night,'path_calibration',path_calib,'path_save',[path_save,'20130801/']);
+    t0 = toc(t0);    
+    p.vizualizeResults('resultsof','psfr');
+end
